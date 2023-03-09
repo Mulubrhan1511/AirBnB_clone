f3448cf35a28cdda701a418fefb4ce610793db9b
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-03-09 12:59:25 +0300

    mule

--- console.py
+++ console.py
@@ -3,13 +3,16 @@ import cmd
 import sys
 import os
 import shlex
+import models
 from models.base_model import BaseModel
 
-classes = {'BaseModel': BaseModel}
+
+
 
 class HBNBCommand(cmd.Cmd):
     """HBNB Class """
     prompt = '(hbnb) '
+    classes = {'BaseModel': BaseModel, 'BaseModel': BaseModel}
     def do_quit(self, argument):
         """ Defines quit option"""
         return True
@@ -25,11 +28,25 @@ class HBNBCommand(cmd.Cmd):
         pass
     def do_create(self, argument):
         """Creates an instance of BaseModel"""
-        if argument in classes:
-            print("mule i am creating")
+        if argument:
+            #if there is an argument
+            if argument in self.classes:
+                get_class = getattr(sys.modules[__name__], argument)
+                mule = get_class()
+                print(mule.id)
+                models.storage.save()
+                return
+            else:
+                print("** class doesn't exist **")
+                return
         else:
-            print("where is the arg")
-        return
-
+            print("** class name missing **")
+    def do_show(self, argument):
+        """Prints the string representation of an instance based on the class name and id"""
+        if argument:
+            #if there is argument
+            pass
+        else:
+            print("** class name missing **")
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
